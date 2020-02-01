c890d383b2429c679d97d3ac447744bf1d5472fe
Sanjay Venkat <sanjayvenkat2000@gmail.com>
2020-02-01 16:11:04 -0500

    Bump version: 0.1.5 → 0.1.6

--- dv_pyclient/__init__.py
+++ dv_pyclient/__init__.py
@@ -2,10 +2,6 @@
 
 __author__ = """Datavore Labs"""
 __email__ = 'info@datavorelabs.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
-from . import dv_pyclient
-
-
-def hello():
-    dv_pyclient.hello()
+from .dv_pyclient import hello
