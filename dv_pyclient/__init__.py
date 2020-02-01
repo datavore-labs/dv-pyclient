4c50aa0da35f6238766eac11ea9178438b58e046
Sanjay Venkat <sanjayvenkat2000@gmail.com>
2020-02-01 16:01:57 -0500

    Bump version: 0.1.4 → 0.1.5

--- dv_pyclient/__init__.py
+++ dv_pyclient/__init__.py
@@ -2,6 +2,10 @@
 
 __author__ = """Datavore Labs"""
 __email__ = 'info@datavorelabs.com'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from . import dv_pyclient
+
+
+def hello():
+    dv_pyclient.hello()
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sanjayvenkat2000/dv_pyclient',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
