d4031432fd021b9b02b21873c83e86c31950c291
dante <c.dante.federici@gmail.com>
2020-04-29 17:07:17 -0400

    Bump version: 0.6.1 → 0.6.2

--- dv_pyclient/__init__.py
+++ dv_pyclient/__init__.py
@@ -23,5 +23,5 @@
 
 __author__ = """Datavore Labs"""
 __email__ = 'info@datavorelabs.com'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
